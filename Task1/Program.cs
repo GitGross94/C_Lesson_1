ab59d79b63f8f979966ba8a020da8452dc4c22ee
Boris <twinlab.forever@yandex.ru>
2022-12-10 18:02:04 +0500

    Задача 2 Пример 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+int max;
+if (a > b) 
+{
+   max = a;    
+}
+else 
+{
+   max = b;    
+}
+Console.WriteLine (max);
