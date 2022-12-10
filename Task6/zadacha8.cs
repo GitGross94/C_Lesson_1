9ba8c32be91832ae19156afdefab15fe86e632b3
Boris <twinlab.forever@yandex.ru>
2022-12-10 19:29:45 +0500

    Задача 6

---
+++ Task6/zadacha8.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+int k = 2;
+            while (k < num)
+            {
+                int result = k % 2;
+                if (result != 1)
+                {
+                    Console.WriteLine("четное " + k);
+                    k++;
+                }
+                else
+                {
+                    k++;
+                }
+            }
