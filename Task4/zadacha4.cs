8fd66e9cee0022a73668062002805d0c25f550e7
Boris <twinlab.forever@yandex.ru>
2022-12-10 18:44:23 +0500

    Задача 4

---
+++ Task4/zadacha4.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine ());
+int max;
+if (a > b & a>c)  
+{
+    max = a;
+    } 
+    else
+    {
+if (b > c)  
+{
+    max = b;
+} 
+else max = c;}
+Console.WriteLine(max);
