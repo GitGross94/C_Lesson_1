8cba7ba7fa8effb35735617b01c3be380a4775fc
Boris <twinlab.forever@yandex.ru>
2022-12-10 18:50:57 +0500

    Задача 6

---
+++ Task5/zadacha6.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+
+if (a % 2 == 0)  
+{
+    Console.WriteLine("Это число четное");
+    } 
+    else
+    {
+Console.WriteLine("Это число нечетное"); 
+    }
+
