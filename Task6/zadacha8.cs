368ea42ff1d01d75ca4f4b199d31f013213b6fc3
Boris <twinlab.forever@yandex.ru>
2022-12-10 19:31:13 +0500

    Задача 8

--- Task6/zadacha8.cs
+++ Task6/zadacha8.cs
@@ -2,8 +2,8 @@
 
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine ());
-int k = 2;
-            while (k < num)
+int k = 1;
+            while (k <= num)
             {
                 int result = k % 2;
                 if (result != 1)
